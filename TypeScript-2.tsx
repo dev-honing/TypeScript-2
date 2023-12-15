424ed40d627936b1be85bf8b4be4ba1320bbe35e
Ho Nyeong <dev.honing@gmail.com>
2023-12-15 23:46:31 +0900

    주석 설명 추가
    - 인덱스 시그니처에 대한 추가적인 학습이 필요

--- TypeScript-2.tsx
+++ TypeScript-2.tsx
@@ -2,9 +2,12 @@
 
 // TypeScript의 인터페이스 정의(interface 키워드)
 interface Props {
+  // '?'는 선택적 속성을 의미한다. 있을 수도/없을 수도 있다.
   id?: string;
-  style?: Partial<CSSStyleDeclaration>;
-  [key: string]: any;
+  style?: Partial<CSSStyleDeclaration>; // CSSStyleDeclaration는 CSS 스타일을 나타내는 타입
+  
+  // 인덱스 시그니처: TypeScript에서 객체의 속성에 동적인 키를 사용할 때 사용하는 문법
+  [key: string]: any; // any를 사용하면 어떤 타입이든 올 수 있다. 모든 타입에 대한 작성을 허용하는 것
 }
 
 // 인터페이스의 사전적인 정의는 '약속'이다.
